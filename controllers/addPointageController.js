f2a115cb30514384b81c1a9157c120a20c8e9910
emmadal <emmanueldalougou@gmail.com>
2023-01-11 08:14:00 +0000

    add vercell config

--- controllers/addPointageController.js
+++ controllers/addPointageController.js
@@ -1,25 +1,29 @@
 const { Pointage } = require("../models/models");
 
 const addPointage = async (req, res, next) => {
-  const { pointage } = req.body;
-  const search = await Pointage.findOne({
+  const search = await Pointage.findAndCountAll({
     where: {
-      date: pointage.date,
-      idculte: pointage.idculte,
-      idmembres: pointage.idmembres,
+      date: req.body.date,
+      idculte: req.body.idculte,
+      idmembres: req.body.idmembres,
     },
   });
-  if (!search) {
+  console.log(search)
+  if (search.count === 0) {
+    const pointage = {
+      date: req.body.date,
+      idculte: req.body.idculte,
+      idmembres: req.body.idmembres,
+      identreprises: req.body.identreprises,
+    };
     await Pointage.create(pointage);
     res.status(200).json({ message: "Bravo, présence confirmée." });
     next();
   } else {
-    res
-      .status(404)
-      .json({
-        status: false,
-        message: "Votre présence a déja eté confirmé",
-      });
+    res.status(404).json({
+      status: false,
+      message: "Votre présence a déja eté confirmé",
+    });
   }
 };
 
