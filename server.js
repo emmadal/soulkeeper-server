9c41040e2d6177b5bdf7dce3ae95dcf55082d3f6
emmadal <emmanueldalougou@gmail.com>
2023-01-11 08:20:54 +0000

    remove swagger

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 require("dotenv").config();
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
 const express = require("express");
 const cors = require("cors");
 
@@ -33,33 +31,6 @@ app.use("/", pointageRoute);
 app.use("/", statsRoute);
 app.use("/", villeRoute);
 app.use("/", userRoute);
-
-
-// Swagger config
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Soulkeeper Express API with Swagger",
-      version: "0.1.0",
-      description:
-        "This is a simple CRUD API application made with Express and documented with Swagger",
-    },
-    servers: [
-      {
-        url: "http://apimobile.bigdata-ci.com",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
-
-const specs = swaggerJsdoc(options);
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs, { explorer: true }),
-);
 
 // Start server
 const port = process.env.PORT || 3000;
