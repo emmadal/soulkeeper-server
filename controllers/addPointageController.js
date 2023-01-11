00541ddb2eff9a44582002fc157cd4effc0cfc8e
emmadal <emmanueldalougou@gmail.com>
2023-01-11 11:11:58 +0000

    change query

--- controllers/addPointageController.js
+++ controllers/addPointageController.js
@@ -1,29 +1,37 @@
 const { Pointage } = require("../models/models");
 
 const addPointage = async (req, res, next) => {
-  const search = await Pointage.findAndCountAll({
-    where: {
-      date: req.body.date,
-      idculte: req.body.idculte,
-      idmembres: req.body.idmembres,
-    },
-  });
-  console.log(search)
-  if (search.count === 0) {
-    const pointage = {
-      date: req.body.date,
-      idculte: req.body.idculte,
-      idmembres: req.body.idmembres,
-      identreprises: req.body.identreprises,
-    };
-    await Pointage.create(pointage);
-    res.status(200).json({ message: "Bravo, présence confirmée." });
-    next();
-  } else {
-    res.status(404).json({
-      status: false,
-      message: "Votre présence a déja eté confirmé",
+  try {
+    const search = await Pointage.findOne({
+      where: {
+        [Op.and]: [
+          { date: req.body.date },
+          { idmembres: req.body.idmembres },
+          { idculte: req.body.idculte },
+        ],
+      },
     });
+    if (search) {
+      res.status(404).json({
+        status: false,
+        message: "Votre présence a déja eté confirmé",
+      });
+      next();
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      const pointage = {
+        date: req.body.date,
+        idculte: req.body.idculte,
+        idmembres: req.body.idmembres,
+        identreprises: req.body.identreprises,
+      };
+      await Pointage.create(pointage);
+      res
+        .status(200)
+        .json({ status: true, message: "Bravo, présence confirmée." });
+    }
+  } catch (error) {
+    console.log("ERROR Add pointage: ", error);
   }
 };
 
